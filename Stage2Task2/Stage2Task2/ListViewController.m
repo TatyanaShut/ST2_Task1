a2e1a51ea71b7cca41aa9af197b5c7c32f9561f4
Tatyana Shut <tatyanashut99@gmail.com>
2019-06-02 17:25:08 +0300

    SecondUIVC is ready

--- Stage2Task2/Stage2Task2/ListViewController.m
+++ Stage2Task2/Stage2Task2/ListViewController.m
@@ -8,24 +8,35 @@
 
 #import "ListViewController.h"
 #import "CustomView.h"
-#define arrayImage [NSArray arrayWithObjects: @"1.jpeg", nil]
+
+
 
 @interface ListViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) NSDictionary* imagesDiscription;
+@property (weak, nonatomic) UILabel* descriptionLabel;
 
 @end
 
 @implementation ListViewController
 
 - (void)viewDidLoad {
-    self.title = @"Application";
+    self.title = @"Select Item";
+    
+    self.imagesDiscription = @{@"1.jpg" : @"url1",
+                               @"2.jpg" : @"url2",
+                               @"3.jpg" : @"url3",
+                               @"4.jpg" : @"url4",
+                               @"5.jpg" : @"url5",
+                               @"6.jpg" : @"url6"
+                               };
+    
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:@"Close"
                                                                   style:UIBarButtonItemStylePlain
                                                                  target:self
                                                                  action:@selector(rightBarButtonItemClick:)];
     [self.navigationItem setRightBarButtonItem:rightItem];
     [self.view addSubview:_scrollView];
-    
     self.navigationItem.leftBarButtonItem=nil;
     self.navigationItem.hidesBackButton=YES;
     
@@ -41,32 +52,46 @@
     [NSLayoutConstraint activateConstraints:
      @[
        
-       [self.scrollView.widthAnchor constraintEqualToConstant:self.view.bounds.size.width/1.2]
+       [self.scrollView.widthAnchor constraintEqualToConstant:self.view.bounds.size.width]
        ]];
     
-    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height*3)];
+    [_scrollView setContentSize:CGSizeMake(0, self.view.frame.size.height*3)];
     
     
     
-    
-    NSMutableArray * viewArray = [[NSMutableArray alloc]init];
-    const int a = 50;
-    for(NSInteger i = 0; i < arrayImage.count; i++){
+    NSInteger counter = 0;
+    NSInteger distance = self.view.frame.size.height/3;
+    for(id i in self.imagesDiscription){
         
-        NSString *name = [[NSString alloc]init];
-        name = arrayImage[i];
-        UIImage *image = [UIImage imageNamed:name];
-        CustomView *view = [[CustomView alloc] initWithImage:image andDescription:@"some url"];
-        [viewArray addObject:view];
-        NSLog(@"%@", viewArray[i]);
-        [_scrollView addSubview:viewArray[i]];
         
-        view.frame = CGRectMake(0, 0+a, image.size.width/2, image.size.height/2);
-        //view.center = self.view.center;
+        UIImage *image = [UIImage imageNamed:i];
+        CGRect ImageRect = CGRectMake(self.view.frame.size.width/5, counter * distance + 15, image.size.width, image.size.height);
+        CGRect rectForDescription = CGRectMake(self.view.frame.size.width/5,counter * distance - 15 + image.size.height, image.size.width, 30);
+        CustomView *view = [[CustomView alloc] initWithImage:image andDescription:[self.imagesDiscription objectForKey:i]];
+        
+        UILabel* descriptionLabel = [[UILabel alloc] initWithFrame:rectForDescription];
+        
+        descriptionLabel.textColor = [UIColor redColor];
+        descriptionLabel.text = [self.imagesDiscription objectForKey:i];
+        
+        
+        UITapGestureRecognizer *singleFingerTap =
+        [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                action:@selector(handleSingleTap:)];
+        [view addGestureRecognizer:singleFingerTap];
+        [_scrollView addSubview:view];
+        [_scrollView addSubview:descriptionLabel];
+        view.frame = ImageRect;
+        
+        counter++;
     }
 }
 
 - (void)rightBarButtonItemClick:(id)send {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+- (void)handleSingleTap:(UITapGestureRecognizer *)recognizer
+{
     [self.navigationController popViewControllerAnimated:YES];
 }
 @end
